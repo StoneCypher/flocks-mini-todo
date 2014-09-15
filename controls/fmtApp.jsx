4c8345a6a5dc1dde42b75519ec47e03f7092ad3f
John Haugeland <stonecypher@gmail.com>
2014-09-15 00:22:16 -0700

    rendering, sorting, styled

--- controls/fmtApp.jsx
+++ controls/fmtApp.jsx
@@ -2,12 +2,94 @@
 
 var fmtApp = React.createClass({
 
-    render: function() {
-        var Rows = [];
-        this.props.data.todoList.map(function(Item) {
-            Rows.push(<div>{Item.item}</div>);
-        });
-        return (<div>{Rows}</div>);
-    }
+
+
+  makeRows: function(InData) {
+
+    var Rows = [];
+
+    InData.map(function(Item) {
+
+      Rows.push(
+        <tr>
+          <td>{Item.item}</td>
+          <td>{Item.priority}</td>
+          <td>{new Date(Item.due).toString()}</td>
+        </tr>
+      );
+
+    });
+
+    return Rows;
+
+  },
+
+
+
+
+  undefComp: function(X,Y) {
+
+    return (X === undefined)? (
+      ( (Y === undefined)?  0 : 1       )
+    ) : (
+      ( (Y === undefined)? -1 : ((X === Y)? 0 : ((X < Y)? -1 : 1)) )
+    );
+
+  },
+
+
+
+  undefDescComp: function(X,Y) {
+
+    return (X === undefined)? (
+      ( (Y === undefined)?  0 : 1       )
+    ) : (
+      ( (Y === undefined)? -1 : ((X === Y)? 0 : ((X < Y)? 1 : -1)) )
+    );
+
+  },
+
+
+
+  todoPriorityPredicate: function(Left, Right) {
+
+    var pri = this.undefDescComp(Left.priority, Right.priority); if (pri !== 0) { return pri; }
+    var due = this.undefComp(    Left.due,      Right.due);      if (due !== 0) { return due; }
+
+    return this.undefComp(Left.item, Right.item);
+
+  },
+
+
+
+  sortRows: function(OldRows) {
+
+    var Rows = OldRows;
+    Rows.sort(this.todoPriorityPredicate);
+
+    return Rows;
+
+  },
+
+
+
+  render: function() {
+
+    var SortedRows = this.makeRows(this.sortRows(this.props.data.todoList));
+
+    return (
+      <table><tbody>
+
+        <tr>
+          <th>Item</th>
+          <th>Priority</th>
+          <th>Due date</th>
+        </tr>
+
+        {SortedRows}
+
+      </tbody></table>
+    );
+  }
 
 });
--- tempdata.js
+++ tempdata.js
@@ -2,13 +2,16 @@
 var TempData = {
 
     lastChecked: undefined,
+
     todoList: [
       { item: 'get milk' },
-      { item: 'homework',    priority: 5, due: new Date().now + 3*24*60*60*1000 },
-      { item: 'fix server',  priority: 9 },
-      { item: 'check alarm', priority: 3 },
-      { item: 'read Candide' },
-      { item: 'get coffee',  priority: 8, due: new Date().now + 4*60*60*1000 }
+      { item: 'homework',     priority: 5, due: new Date().getTime() + 3*24*60*60*1000 },
+      { item: 'net homework', priority: 2, due: new Date().getTime() + 3*24*60*60*1000 },
+      { item: 'end homework', priority: 5, due: new Date().getTime() + 6*24*60*60*1000 },
+      { item: 'fix server',   priority: 9 },
+      { item: 'check alarm',  priority: 3 },
+      { item: 'read Candide'  },
+      { item: 'get coffee',   priority: 8, due: new Date().getTime() + 4*60*60*1000 }
     ]
 
 };
